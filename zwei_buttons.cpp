e7d462cd097a306aaca07f17d4fe0e83d5914cda
allipierre <yottiallipierre40@yahoo.fr>
2015-07-09 01:51:40 -0700

    ff

--- zwei_buttons.cpp
+++ zwei_buttons.cpp
@@ -3,6 +3,35 @@
 zwei_buttons::zwei_buttons(QWidget *parent)
     : QMainWindow(parent)
 {
+   this->mywidget=new QWidget();
+    this->mywidget->setGeometry(200,100,450,150);
+    this->ester_button=new QPushButton("Quit(pressed():schon beim Mausclick",this->mywidget);
+    this->ester_button->setGeometry(30,10,400,30);
+
+    /*
+     * QApplication::instance() kann damit das Programm beim pressed schließen
+     * */
+    connect(this->ester_button,SIGNAL(pressed()),QApplication::instance(),SLOT(quit()));
+
+    this->mylabel=new QLabel("Ein Programm mit zwei Quit-buttons",this->mywidget);
+    this->mylabel->setFont(QFont("Times",18,QFont::Bold));
+
+    this->zweiter_button=new QPushButton("Quit(cliked():erst nach click und los lassen den Maus",this->mywidget);
+    this->zweiter_button->setGeometry(30,90,400,50);
+    connect(this->zweiter_button,SIGNAL(clicked()),QApplication::instance(),SLOT(quit()));
+
+    this->mylabel->setGeometry(30,40,400,50);
+    this->vLayout=new QVBoxLayout();
+
+
+    this->vLayout->addWidget(this->ester_button);
+    this->vLayout->addWidget(this->mylabel);
+    this->vLayout->addWidget(this->zweiter_button);
+   // this->vLayout->addWidget(this->lbEncriptionOutput);
+    mywidget->setLayout(this->vLayout);
+    this->setCentralWidget(mywidget);
+
+
 }
 
 zwei_buttons::~zwei_buttons()
--- zwei_buttons.h
+++ zwei_buttons.h
@@ -1,8 +1,11 @@
 #ifndef ZWEI_BUTTONS_H
 #define ZWEI_BUTTONS_H
-
+#include <QApplication>
 #include <QMainWindow>
-
+#include<QLabel>
+#include<QLayout>
+#include<QPushButton>>
+#include<QFont>
 class zwei_buttons : public QMainWindow
 {
     Q_OBJECT
@@ -10,6 +13,13 @@ class zwei_buttons : public QMainWindow
 public:
     zwei_buttons(QWidget *parent = 0);
     ~zwei_buttons();
+private:
+    QVBoxLayout *vLayout;
+    QWidget *mywidget;
+    QPushButton *ester_button;
+    QPushButton *zweiter_button;
+    QLabel *mylabel;
+
 };
 
 #endif // ZWEI_BUTTONS_H
